a94f730632074d79a01fb6ca8cac549c4ae4ca63
Jack <jackhasakeyboard@gmail.com>
2023-03-19 23:33:38 -0600

    Generated input wrapped in brackets

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=gcc
 
-EXEC=a.out
+EXEC=wrap
 
 BUILDDIR=build
 
--- main.c
+++ main.c
@@ -1,3 +1,35 @@
+#include <stdio.h>
+
+enum {
+	OPEN,
+	CLOSE
+};
+
+const char pair[2] = {
+	'(',
+	')'
+};
+
+void err(char* msg) {
+	printf("Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	char* buff = argv[1];
+
+	printf("%c%s%c\n", pair[OPEN], buff, pair[CLOSE]);
+
 	return 0;
 }
